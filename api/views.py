86942969463def4d671702d9b0d47b77fbd2c4ff
Nadhil-an <nadhil.edu@gmail.com>
2026-01-18 00:07:25 +0530

    make changes in api formate

--- api/views.py
+++ api/views.py
@@ -1,14 +1,26 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from student.models import Student
+from .serializers import StudentSerializer
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view
 
 # Create your views here.
 
+@api_view(['GET','POST'])
 def studentsViews(request):
-    student = [
-        {
-            'id':'1',
-            'name':'nadhil',
-            'course':'cs'
-        }
-    ]
-    return JsonResponse(student,safe=False)
+    if request.method == 'GET':
+
+        student = Student.objects.all()
+        serializer = StudentSerializer(student, many=True)
+        return Response(serializer.data,status=status.HTTP_200_OK)
+    elif request.method == 'POST':
+
+        serializer = StudentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        print(serializer.errors)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
--- student/views.py
+++ student/views.py
@@ -4,7 +4,5 @@ from django.http import HttpResponse
 # Create your views here.
 def students(request):
 
-    student = [
-        {'id':1,'name':'nadhil','course':'bca'}
-    ]
+   
     return HttpResponse(student)
