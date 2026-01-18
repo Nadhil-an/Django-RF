30b9afa1a8a6b1a479e6b585f1a2d9dd119e32be
Nadhil-an <nadhil.edu@gmail.com>
2026-01-19 01:19:50 +0530

    creating class based function

--- api/urls.py
+++ api/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns =[
     path('student/',views.studentsViews),
     path('student/<int:pk>/',views.studentDetailview),
+
+    path('employee/',views.Employees.as_view())
+
 ]
--- api/views.py
+++ api/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from student.models import Student
-from .serializers import StudentSerializer
+from employee.models import Employee
+from .serializers import StudentSerializer,EmployeeSerializer
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view
+from rest_framework.decorators import APIView
+
 
 # Create your views here.
 
@@ -46,3 +49,16 @@ def studentDetailview(request, pk):
     elif request.method == 'DELETE':
         student.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+    
+class Employees(APIView):
+    def get(self, request):
+        employees = Employee.objects.all()
+        serializer = EmployeeSerializer(employees, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def post(self, request):
+        employees = EmployeeSerializer(data=request.data)
+        if employees.is_valid():
+            employees.save()
+            return Response(employees.data,status=status.HTTP_201_CREATED)
+        return Response(employees.errors,status=status.HTTP_400_BAD_REQUEST)
